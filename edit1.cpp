2df49e7cee2cfd699ae5eae24eff32df2adf2f39
DenzelGurung <denzelgrg001@gmail.com>
2023-09-27 23:13:07 -0600

    commit

--- edit1.cpp
+++ edit1.cpp
@@ -3,5 +3,8 @@ using namespace std;
 
 int main(){
     char c;
-    cout<<"what will you choose "
+    cout<<"Rock, Paper, Scissors!"<<endl;
+    cout<<"Choose one of the options"<<endl;
+    cout<<"(R)ock, (P)aper, (S)cissors"<<endl;
+    cin>>c;
 }
